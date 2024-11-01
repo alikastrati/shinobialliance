51c6faeae1cdd8c7baadae90bfdcc896099060ce
alikastrati <93733635+alikastrati@users.noreply.github.com>
2024-11-01 11:40:34 +0100

    enemies die here

--- shinobialliance/Assets/Scripts/PlayerControll Scripts/PlayerController.cs
+++ shinobialliance/Assets/Scripts/PlayerControll Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections; // Add this line
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -12,12 +13,23 @@ public class PlayerController : MonoBehaviour
 
     private Animator animator;
 
+    public float attackRange = 1f; // Set the attack range
+    public float attackDuration = 0.5f; // Duration of the attack
+    private BoxCollider2D attackCollider; // Reference to attack collider
+
+
     private void Start()
     {
         // Get the animator component attached to Player
         animator = GetComponent<Animator>();
         rigidBodyVar = GetComponent<Rigidbody2D>();
-            
+
+        // Create and configure the attack collider
+        attackCollider = gameObject.AddComponent<BoxCollider2D>();
+        attackCollider.size = new Vector2(attackRange, attackRange); // Set the attack collider size
+        attackCollider.isTrigger = true; // Set as trigger
+        attackCollider.enabled = false; // Disable initially
+
     }
 
     //private void Update()
@@ -32,10 +44,38 @@ public class PlayerController : MonoBehaviour
     //    HandleAnimation();
     //}
 
+    public void Attack()
+    {
+        StartCoroutine(AttackCoroutine());
+    }
+
     private void Update()
     {
         moveInput = new Vector2(joystick.Horizontal, joystick.Vertical);
         HandleAnimation();
+    }
+
+    private IEnumerator AttackCoroutine()
+    {
+        // Enable the attack collider
+        attackCollider.enabled = true;
+
+        // Play attack animation (optional)
+        animator.SetTrigger("Attack"); // Ensure you have an attack trigger in your Animator
+
+        // Wait for the duration of the attack
+        yield return new WaitForSeconds(attackDuration);
+
+        // Disable the attack collider
+        attackCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy")) // Make sure enemies are tagged as "Enemy"
+        {
+            Destroy(collision.gameObject); // Destroy the enemy
+        }
     }
 
     private void FixedUpdate()
@@ -80,9 +120,7 @@ public class PlayerController : MonoBehaviour
                 }
                 else
                 {
-
                     animator.SetBool("IsMovingLeft", false);
-
                 }
             }
             else
